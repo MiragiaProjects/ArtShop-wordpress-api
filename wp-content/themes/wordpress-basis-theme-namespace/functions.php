407b32f3b5ccf7cd6a887270d4a154acfa101b7b
Malin Olsson <miragia.solinde@gmail.com>
2023-01-21 21:42:27 +0100

    added custom fields and posrtype to theme

--- wp-content/themes/wordpress-basis-theme-namespace/functions.php
+++ wp-content/themes/wordpress-basis-theme-namespace/functions.php
@@ -82,3 +82,76 @@ if ( ! function_exists( 'wp_basis_load_files' ) ) {
 
 	}
 }
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Products.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Products", "custom-post-type-ui" ),
+		"singular_name" => esc_html__( "Product", "custom-post-type-ui" ),
+		"menu_name" => esc_html__( "My Products", "custom-post-type-ui" ),
+		"all_items" => esc_html__( "All Products", "custom-post-type-ui" ),
+		"add_new" => esc_html__( "Add new", "custom-post-type-ui" ),
+		"add_new_item" => esc_html__( "Add new Product", "custom-post-type-ui" ),
+		"edit_item" => esc_html__( "Edit Product", "custom-post-type-ui" ),
+		"new_item" => esc_html__( "New Product", "custom-post-type-ui" ),
+		"view_item" => esc_html__( "View Product", "custom-post-type-ui" ),
+		"view_items" => esc_html__( "View Products", "custom-post-type-ui" ),
+		"search_items" => esc_html__( "Search Products", "custom-post-type-ui" ),
+		"not_found" => esc_html__( "No Products found", "custom-post-type-ui" ),
+		"not_found_in_trash" => esc_html__( "No Products found in trash", "custom-post-type-ui" ),
+		"parent" => esc_html__( "Parent Product:", "custom-post-type-ui" ),
+		"featured_image" => esc_html__( "Featured image for this Product", "custom-post-type-ui" ),
+		"set_featured_image" => esc_html__( "Set featured image for this Product", "custom-post-type-ui" ),
+		"remove_featured_image" => esc_html__( "Remove featured image for this Product", "custom-post-type-ui" ),
+		"use_featured_image" => esc_html__( "Use as featured image for this Product", "custom-post-type-ui" ),
+		"archives" => esc_html__( "Product archives", "custom-post-type-ui" ),
+		"insert_into_item" => esc_html__( "Insert into Product", "custom-post-type-ui" ),
+		"uploaded_to_this_item" => esc_html__( "Upload to this Product", "custom-post-type-ui" ),
+		"filter_items_list" => esc_html__( "Filter Products list", "custom-post-type-ui" ),
+		"items_list_navigation" => esc_html__( "Products list navigation", "custom-post-type-ui" ),
+		"items_list" => esc_html__( "Products list", "custom-post-type-ui" ),
+		"attributes" => esc_html__( "Products attributes", "custom-post-type-ui" ),
+		"name_admin_bar" => esc_html__( "Product", "custom-post-type-ui" ),
+		"item_published" => esc_html__( "Product published", "custom-post-type-ui" ),
+		"item_published_privately" => esc_html__( "Product published privately.", "custom-post-type-ui" ),
+		"item_reverted_to_draft" => esc_html__( "Product reverted to draft.", "custom-post-type-ui" ),
+		"item_scheduled" => esc_html__( "Product scheduled", "custom-post-type-ui" ),
+		"item_updated" => esc_html__( "Product updated.", "custom-post-type-ui" ),
+		"parent_item_colon" => esc_html__( "Parent Product:", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "Products", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "products",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "products", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail", "excerpt", "custom-fields", "author" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "products", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
